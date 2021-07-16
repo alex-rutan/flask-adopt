aa927b77cd813337f5d3e54fd4ff61331aa1f38b
Loni <kuangloni@gmail.com>
2021-07-16 11:27:33 -0700

    created pet from

--- forms.py
+++ forms.py
@@ -1,1 +1,19 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets."""
+    
+    name = StringField("Pet Name")
+    
+    species = StringField("Species")
+    
+    photo_url = StringField("Photo URL")
+    
+    age = StringField("Age")
+    
+    notes = TextAreaField("Notes")
+
+
